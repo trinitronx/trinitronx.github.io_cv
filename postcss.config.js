852000870dfa722f3edbf3f99db3012ed9226ac1
James Cuzella <james.cuzella@lyraphase.com>
2025-08-22 13:52:18 -0600

    Revert "postcss: Turn off prefers-color-scheme-query (try fixing prefers-contrast issue)"
    This reverts commit 4621fbd7fa843cd3e3e70a4ee30c8c9c91ef375d.

--- postcss.config.js
+++ postcss.config.js
@@ -15,7 +15,7 @@ module.exports = {
                 'overflow-property': true,
                 'overflow-wrap-property': true,
                 'system-ui-font-family': true,
-                'prefers-color-scheme-query': false,
+                'prefers-color-scheme-query': true,
                 'text-decoration-shorthand': true,
                 'blank-pseudo-class': false,
                 'focus-visible-pseudo-class': false,
