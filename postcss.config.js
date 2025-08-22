2852cb28cbbf863405f78b9c7ce795d6487ec53a
James Cuzella <james.cuzella@lyraphase.com>
2025-08-22 13:46:20 -0600

    postcss: Fix indent for plugins config section

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const postcssPresetEnv = require('postcss-preset-env');
 const cssNano = require('cssnano');
 
 module.exports = {
-	plugins: [
+    plugins: [
         postcssPresetEnv({
             env: process.env.NODE_ENV === 'production' ? 'production' : 'development',
             debug: process.env.NODE_ENV === 'development',
